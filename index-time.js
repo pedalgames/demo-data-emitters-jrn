73c952a472f5553a8bacbe01994fc2265e0caf50
Jesper Rosenlund Nielsen <16505099+jeroni7100@users.noreply.github.com>
2024-02-25 13:15:02 +0100

    Initial commit

---
+++ index-time.js
@@ -0,0 +1,28 @@
+// function getTimeString() to get the current time and format it as a string in the format "HH:MM:SS", then send it as a POST request to the plugin server at http://127.0.0.1:49999/plugin?name=texts. The server will then display the time string in the plugin UI.
+
+// Function to get the current time and format it as a string
+const getTimeString = async () => {
+  try {
+    const now = new Date();
+    const timeString = now.toLocaleTimeString([], { hour12: false });
+    console.log(timeString);
+
+    // Send the time string as a POST request
+      const postResponse = await fetch('http://127.0.0.1:49999/plugin?name=texts', {  
+          method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ time: timeString })
+            
+      });
+      console.log('POST request sent successfully');
+  }
+  catch (error) {
+    console.error(error);
+  }
+};
+
+
+// Fetch a new quote every 10 seconds
+setInterval(getTimeString, 10000);
